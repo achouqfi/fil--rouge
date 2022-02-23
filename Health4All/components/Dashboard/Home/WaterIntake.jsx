57dab50df37e96ebf805d0436d67ca25877ca560
achouqfi <75440007+achouqfi@users.noreply.github.com>
2022-02-23 22:34:57 +0100

    progress

---
+++ Health4All/components/Dashboard/Home/WaterIntake.jsx
@@ -0,0 +1,28 @@
+import React, {useEffect, useRef} from 'react'
+import {
+    StyleSheet,
+    View,
+    Text,
+    TouchableOpacity,
+    Animated,
+    SafeAreaView
+} from 'react-native';
+import Svg, { Path,Circle, Mask, G } from "react-native-svg"
+
+export default function WaterIntake() {
+  return (
+    <View style={StyleSheet.container}>
+        <View>
+            <View></View>
+        </View>
+        <View>
+            <View>
+                
+            </View>
+            <View></View>
+            <View></View>
+            <View></View>
+        </View>
+    </View>
+  )
+}
--- Health4All/screens/Dashboard/Home/Home.jsx
+++ Health4All/screens/Dashboard/Home/Home.jsx
@@ -12,6 +12,8 @@ import {
 import StatHeaderHome from '../../../components/Dashboard/Home/ChartHeaderHome'
 import Header from '../../../components/Dashboard/Home/Header'
 import BtnHome from '../../../components/Dashboard/Home/BtnHome'
+import WaterIntake from '../../../components/Dashboard/Home/WaterIntake'
+
 export default function Home() {
   return (
     <ScrollView style={styles.container}>
@@ -20,17 +22,16 @@ export default function Home() {
         <View style={styles.todayTarget}>
           <Text style={styles.HomeTargerDay}>Today Target</Text>
           <View style={styles.btnHome}>
-            <BtnHome  btnTitle={"Check"} color1={"#92A3FD"} color2={'#9DCEFF'} />
+            <BtnHome btnTitle={"Check"} color1={"#92A3FD"} color2={'#9DCEFF'} />
           </View>
         </View>
         <View style={styles.ActivityStatus}>
           <Text style={styles.ActivityStatusTitle}>Activity Status</Text>
           <View style={styles.HeartRate}>
-
           </View>
           <View style={styles.ActivityStatus2}>
             <View style={styles.HeartRatWaterIntakee}>
-                <Text>eeeeee</Text>
+                <WaterIntake />
             </View>
             <View style={styles.SleepCalories}>
               <View style={styles.Sleep}>
@@ -107,7 +108,25 @@ const styles = StyleSheet.create({
       backgroundColor:'white',
       width:'45%',
       marginLeft:'10%',
+    },
+    Sleep:{
+      height:147,
+      backgroundColor:'white',
       borderRadius:16,
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.23,
+      shadowRadius: 2.62,
+      elevation: 4,
+    },
+    Calories:{
+      height:147,
+      backgroundColor:'white',
+      borderRadius:16,
+      marginTop:20,
       shadowColor: "#000",
       shadowOffset: {
         width: 0,
